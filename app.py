7054fc272918fd4cd1782e9f094c119efd210aeb
Okan Uzun <88987740+oqanxc@users.noreply.github.com>
2026-08-12 13:43:07 +0300

    test: test to observe PR flow

--- app.py
+++ app.py
@@ -44,3 +44,5 @@ if __name__ == "__main__":
     init_db()
     #  0.0.0.0 listening for Docker container compatilibity.
     app.run(host="0.0.0.0", port=5000)  # nosec B104
+
+    #  test to observe PR flow 
